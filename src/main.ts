53c98a0ccda6133ea603c7e7e9ddcfd40350111e
dmsvk0217rd <dmsvk0217@reindeers.com>
2025-02-26 19:03:34 +0900

    setting scheduling

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NoticeService } from './notice/notice.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-
-  const noticeService = app.get(NoticeService);
-  await noticeService.checkNotices();
 }
 bootstrap();
-510;
